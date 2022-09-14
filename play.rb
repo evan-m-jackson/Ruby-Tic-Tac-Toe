7d413fae852734ddbfd6ce6434a20c04fcb1f5ca
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2022-09-14 07:50:26 -0400

    Update Play file
    I updated the 'play.rb' file to fix a typo on Lines 21 & 22.

--- play.rb
+++ play.rb
@@ -1,33 +1,33 @@
-require './ticTacToe'
+require './ticTacToe.rb'
 
 game = Game.new
 
 game.welcome
 
 while !game.game_over && !game.player_wins
-  game.print_board
+    game.print_board
+    
+    
+    choice = game.get_player_input
 
-  choice = game.get_player_input
+    if game.is_user_input_valid(choice) && game.is_space_free(choice)
+        game.mark_board(choice)
+        game.change_marker
+        game.cpu_pick
 
-  if game.is_user_input_valid(choice) && game.is_space_free(choice)
-    game.mark_board(choice)
-    game.change_marker
-    game.cpu_pick
+    else
+        game.move_confirmation(choice)
+    end
 
-  else
-    game.move_confirmation(choice)
-  end
 
-  1
-  1
 
 end
 
 game.print_board
 
 if game.player_wins
-  puts "GAME OVER! Player #{game.winning_player} wins!"
+    puts "GAME OVER! Player #{game.winning_player} wins!"
 
 else
-  puts "GAME OVER! It's a tie :("
+    puts "GAME OVER! It's a tie :("
 end
