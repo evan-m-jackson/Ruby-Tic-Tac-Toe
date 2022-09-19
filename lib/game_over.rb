6ea18680701862a3384256c6c2efe7953c0ebea9
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2022-09-19 11:42:10 -0400

    Added a GameOver class

---
+++ lib/game_over.rb
@@ -0,0 +1,25 @@
+class GameOver
+
+    def initialize
+        @WINNING_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    end
+
+    def is_game_over board
+        (1..9).each do |n|
+            return false if board[n - 1] == n.to_s
+        end
+        true
+    end
+
+    def which_player_wins board
+        @WINNING_COMBINATIONS.each do |arr|
+          return board[arr[0]] if board[arr[0]] == board[arr[1]] && board[arr[1]] == board[arr[2]]
+        end
+        'No winner'
+    end
+    
+    def does_a_player_win board
+        which_player_wins(board) != 'No winner'
+    end
+
+end
