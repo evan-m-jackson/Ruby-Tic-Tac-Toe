aa52b4224b0c8daf0038b4904f1148c1ce83c162
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2022-09-19 13:24:01 -0400

    Added tests to Board spec file

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,5 +14,23 @@ describe 'Board' do
       @board.mark_board('5', 'X')
       expect(@board.board[4]).to eq('X')
     end
+
+    it 'Mark the first spot on the board' do
+    
+        @board.mark_board('1', 'X')
+        expect(@board.board).to eq(%w[X 2 3 4 5 6 7 8 9])
+      end
+    
+      it 'Try to mark 6 spot with O' do
+        @board.mark_board('6', 'O')
+        expect(@board.board).to eq(%w[1 2 3 4 5 O 7 8 9])
+      end
+    
+      it 'Player X picks then Player O picks' do
+        
+        @board.mark_board('4','X')
+        @board.mark_board('3','O')
+        expect(@board.board).to eq(%w[1 2 O X 5 6 7 8 9])
+      end
   
   end
