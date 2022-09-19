220a1529083527f5ea22c268ce80e73cff7cfc85
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2022-09-19 13:01:12 -0400

    Updated PlayerChoice class and test files

--- lib/player_choice.rb
+++ lib/player_choice.rb
@@ -1,4 +1,18 @@
 class PlayerChoice
+
+    def initialize
+        @HUMAN = 'X'
+    end
+
+    def selection_message
+        puts "\nPlease choose a spot on the board between 1-9: \r"
+      end
+    
+      def get_player_input
+        selection_message
+        gets.chomp.to_s
+      end
+
     def is_user_input_valid(choice)
         if choice !~ /\D/ && choice.length > 0
           num = choice.to_i
@@ -9,6 +23,21 @@ class PlayerChoice
           end
         else
           false
+        end
+      end
+
+    def is_space_free(board, choice)
+        num = choice.to_i
+        choice == board[num - 1]
+    end
+
+    def move_confirmation(board, choice)
+        if !is_user_input_valid(choice)
+          puts "Sorry that is not a valid move. Please select an integer between 1 and 9.\n"
+        elsif !is_space_free(board, choice)
+          puts 'Sorry this space is taken. Please select another.'
+        else
+          puts 'Thank you!'
         end
       end
 end
--- spec/player_choice_spec.rb
+++ spec/player_choice_spec.rb
@@ -32,4 +32,19 @@ describe 'PlayerChoice' do
       expect(@choice.is_space_free(board, choice)).to be false
     end
 
+    it 'message prints if a spot is taken' do
+        board = %w[X 2 3 4 5 6 7 8 9]
+        expect do
+          @choice.move_confirmation(board, '1')
+        end.to output("Sorry this space is taken. Please select another.\n").to_stdout
+      end
+    
+    it 'message prints for an invalid choice' do
+        board = Board.new.board
+        expect do
+          @choice.move_confirmation(board, 'a')
+        end.to output("Sorry that is not a valid move. Please select an integer between 1 and 9.\n").to_stdout
+      end
+    
+
   end
