42a4c9aa367611f1f36ca0ab4e4c1bf99c6bfbe3
evan-m-jackson <90336648+evan-m-jackson@users.noreply.github.com>
2022-09-21 11:49:46 -0400

    Refactored Board spec file

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,28 +6,30 @@ describe 'Board' do
     @board = Board.new
   end
 
-  it 'Has String array (aka the board)' do
-    expect(@board.board).to eq(%w[1 2 3 4 5 6 7 8 9])
-  end
+  describe '.mark_board' do
+    it 'Has String array (aka the board)' do
+      expect(@board.board).to eq(%w[1 2 3 4 5 6 7 8 9])
+    end
 
-  it 'Mark the board in the middle spot(5) with an X' do
-    @board.mark_board('5', 'X')
-    expect(@board.board[4]).to eq('X')
-  end
+    it 'Mark the board in the middle spot(5) with an X' do
+      @board.mark_board('5', 'X')
+      expect(@board.board[4]).to eq('X')
+    end
 
-  it 'Mark the first spot(1) on the board' do
-    @board.mark_board('1', 'X')
-    expect(@board.board).to eq(%w[X 2 3 4 5 6 7 8 9])
-  end
+    it 'Mark the first spot(1) on the board' do
+      @board.mark_board('1', 'X')
+      expect(@board.board).to eq(%w[X 2 3 4 5 6 7 8 9])
+    end
 
-  it 'Mark the 6 spot with an O' do
-    @board.mark_board('6', 'O')
-    expect(@board.board).to eq(%w[1 2 3 4 5 O 7 8 9])
-  end
+    it 'Mark the 6 spot with an O' do
+      @board.mark_board('6', 'O')
+      expect(@board.board).to eq(%w[1 2 3 4 5 O 7 8 9])
+    end
 
-  it 'Player X picks then Player O picks' do
-    @board.mark_board('4', 'X')
-    @board.mark_board('3', 'O')
-    expect(@board.board).to eq(%w[1 2 O X 5 6 7 8 9])
+    it 'Player X picks then Player O picks' do
+      @board.mark_board('4', 'X')
+      @board.mark_board('3', 'O')
+      expect(@board.board).to eq(%w[1 2 O X 5 6 7 8 9])
+    end
   end
 end
